ada88bdfb3f6b00af81ec87bd9f28f8be1b9a409
keitaroryu <keitaroryu@gmail.com>
2019-12-01 22:10:51 -0600

    Version 6. Mute Button Interaction added. Removed Felyne-GIF.gif. Removed Code using removed gif.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 window.addEventListener("load", function() {
+
     let muteButton = document.getElementById("mute");
 
     muteButton.addEventListener("click", function(){
@@ -6,9 +7,19 @@ window.addEventListener("load", function() {
 
         if(video.muted){
             video.muted = false;
+            muteButton.innerHTML = "STOP ME NYA~!!!!!!";
         } else {
             video.muted = true;
+            muteButton.innerHTML = "Listen to the call of my people!";
         };
+    });
+
+    muteButton.addEventListener("mouseover", function(){
+        muteButton.style.color = "white";
+    });
+
+    muteButton.addEventListener("mouseout", function(){
+        muteButton.style.color = "#c3c3c3";
     });
 
 });
